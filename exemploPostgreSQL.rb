79b2b0762d1a35aeddfe826785c763245bbc6194
IgorAvilaPereira <igor.pereira@riogrande.ifrs.edu.br>
2016-11-16 18:49:16 -0200

    teste postgres

---
+++ exemploPostgreSQL.rb
@@ -0,0 +1,22 @@
+# http://stackoverflow.com/questions/3116015/how-to-install-postgresqls-pg-gem-on-ubuntu
+# sudo apt-get install libpq-dev
+# gem install pg  --   --with-pg-lib=/usr/lib   
+
+# https://deveiate.org/code/pg/PG/Connection.html
+
+require 'pg'
+
+# teste conexao
+conn = PGconn.connect("localhost", 5432, '', '', "test", "postgres", "postgres")
+
+# http://stackoverflow.com/questions/4683057/simple-example-of-postgres-query-in-ruby
+# teste select
+res  = conn.exec('select * from department')
+
+res.each do |row|
+  puts row['id'] + ' | ' + row['name']
+end
+
+# http://www.rubydoc.info/gems/pg/PG/Connection
+# insert
+res = conn.exec_params('INSERT INTO department (name) VALUES ($1)', ['Ruby'])
