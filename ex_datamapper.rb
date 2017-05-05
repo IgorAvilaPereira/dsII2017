16bbc0a0507b3549ab65b788abc61ba469a86e5b
IgorAvilaPereira <igor.pereira@riogrande.ifrs.edu.br>
2017-05-04 22:46:41 -0300

    data

---
+++ ex_datamapper.rb
@@ -0,0 +1,56 @@
+require 'rubygems'
+
+# sudo apt-get install postgresql-server-dev-all 
+# sudo apt-get install libpq-dev
+# sudo gem install dm-postgres-adapter
+# sudo gem install data_mapper
+
+require 'data_mapper' 
+
+require 'dm-migrations'
+
+# Conexao PostgreSQL:
+DataMapper.setup(:default, 'postgres://postgres:postgres@localhost/testactiverecord')
+ 
+# Conexao SQL3lite
+#DataMapper.setup(:default, 'sqlite:///home/iapereira/project.db')
+
+# modelo
+class Post
+  include DataMapper::Resource
+  property :id,         Serial    # An auto-increment integer key
+  property :title,      String    # A varchar type string, for short strings
+  property :body,       Text      # A text block, for longer string data.
+  property :created_at, DateTime  # A DateTime, for any date you might like.
+end
+
+# finaliza o bloco de descricao das tabelas/modelos
+DataMapper.finalize
+
+# criar as tabelas
+# DataMapper.auto_migrate!
+
+# destruir coisas velhas e criar as novas tabelas
+DataMapper.auto_upgrade!
+
+
+# adicionar
+@post = Post.new(:title => "testedatamappper")
+@post.save # persist the resource
+
+# remover
+post2 = Post.get(1)
+if post2.nil? == false 
+	post2.destroy  # => true
+end
+
+# listar
+vetPost = Post.all
+vetPost.each do |p|
+	puts p.title
+end
+
+# atualizar
+post3 = Post.get(2)
+post3.update(:title => 'Funky Town Municipal Zoo')
+
